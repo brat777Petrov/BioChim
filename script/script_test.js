aa0d4b81d810bbca70692f920eca9fc449f33d74
Sasha Petrov <brat77@tut.by>
2021-02-06 13:00:37 +0300

    fix script bug with img

--- script/script_test.js
+++ script/script_test.js
@@ -9,13 +9,14 @@ let  response =  await fetch('../tests/biolog_test_1.json');
 
 let base =  await response.json();
 
-//***************** работа с вопросами */   console.log(base); 
+//***************** работа с вопросами */    console.log(); 
 
  
   
   print_question (base.bullet[i].question);
+  print_img (base.bullet[i].picture);
 
-i++;
+  i++;
 
 return ;
 }
@@ -25,7 +26,13 @@ function print_question(i) {
 };
 
 
-function print_img() {};
+function print_img(path) {
+  let test_img = '<img src="'+path+'" >';
+  
+  
+  $('div.test_img').html('');
+  $('div.test_img').append(test_img);
+};
 
 
 function print_answers() {};
