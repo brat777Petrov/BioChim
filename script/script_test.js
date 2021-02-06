520b7a0cd55640f3e392541ef6dbc375cfb4c287
Sasha Petrov <brat77@tut.by>
2021-02-06 11:39:59 +0300

    add script(test_1)+json (test_1)

--- script/script_test.js
+++ script/script_test.js
@@ -1,4 +1,31 @@
-function next() {
-  return console.log('777');
+let i = 0;// номер вопроса number question
+next();
+
+async function next() {
+
+//************** Подключение JSON файла 
+                     
+let  response =  await fetch('../tests/biolog_test_1.json');
+
+let base =  await response.json();
+
+//***************** работа с вопросами */   console.log(base); 
+
+ 
+  
+  print_question (base.bullet[i].question);
+
+i++;
+
+return ;
 }
 
+function print_question(i) {
+  $('div.test_question').html(i);
+};
+
+
+function print_img() {};
+
+
+function print_answers() {};
