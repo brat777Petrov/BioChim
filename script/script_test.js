5a75c5053b4cd94854f949b7f633826df174a910
Sasha Petrov <brat77@tut.by>
2021-02-07 13:56:59 +0300

    add input form, add animation in test jQuery

--- script/script_test.js
+++ script/script_test.js
@@ -1,5 +1,5 @@
-let i = 0;// номер вопроса number question
-next();
+let i = 0;// number question
+next();//first question print
 
 async function next() {
 
@@ -9,14 +9,15 @@ let  response =  await fetch('../tests/biolog_test_1.json');
 
 let base =  await response.json();
 
-//***************** работа с вопросами */    console.log(); 
+//***************** работа с вопросами 
 
  
   
   print_question (base.bullet[i].question);
   print_img (base.bullet[i].picture);
-
-  i++;
+  print_answers (base.bullet[i].typeOFanswers, base.bullet[i].answers);
+  save_result();
+  i++; //next question
 
 return ;
 }
@@ -27,12 +28,40 @@ function print_question(i) {
 
 
 function print_img(path) {
+  if (path=='') { 
+    $('.test_img').slideUp(500);
+  } else {
+    $('.test_img').slideDown(500);
+  }
+  
   let test_img = '<img src="'+path+'" >';
-  
-  
+    
   $('div.test_img').html('');
   $('div.test_img').append(test_img);
 };
 
 
-function print_answers() {};
+function print_answers(typeOFanswers, answers) {
+
+  if (typeOFanswers == "button") {
+
+    $('div.test_answer').html('');
+    
+    for(let i = 0; i < 4; i++) {
+      let answer = '<div class="answer_button">'+answers[i]+'</div>';
+      $('div.test_answer').append(answer);
+        }
+
+  } else {
+    
+    $('div.test_answer').html('');
+    let answer = '<input autofocus type="text">'
+    $('div.test_answer').append(answer);
+  }
+
+
+};
+
+save_result() {
+  
+}
