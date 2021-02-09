672cb3a786a6ac2b022134775e7030cf2947b23e
Sasha Petrov <brat77@tut.by>
2021-02-09 21:45:52 +0300

    add total result

--- script/script_test.js
+++ script/script_test.js
@@ -25,10 +25,10 @@ let base =  await response.json();
   print_answers (base.bullet[i].typeOFanswers, base.bullet[i].answers);
   save_result(base.bullet[i].typeOFanswers);
   baseResult.push(localStorage.result);
-  
+ 
   i++; //next question
 
-  if (i == 4) {
+  if (i == 7) {
     print_result(base, baseResult);
    
   }
@@ -48,13 +48,20 @@ function print_img(path) {
     $('.test_img').slideUp(500);
   } else {
     $('.test_img').slideDown(500);
-  }
+     
   
-  let test_img = '<img src="'+path+'" >';
+  let test_img = '<img src="'+path+'" id="test_img1">';
     
   $('div.test_img').html('');
   $('div.test_img').append(test_img);
+
+  //const el = document.getElementById('sgf');//? window scroll
+  //console.log(el);
+  //el.scrollIntoView();
+   //window.scrollTo(0,1000);
   
+
+  }
   
 };
 
@@ -109,22 +116,51 @@ function print_result (base,baseResult) {
   let field = $('div.area_test');
 
   field.append('<div class="result question_title">Р Е З У Л Ь Т А Т Ы</div>');
+ 
+  //*** Print  - button *******************************************/
+ 
+  var total_right = 0;
+  for (let j = 0; j < baseResult.length-1; j++) {
 
-  for (let j = 0; j < baseResult.length; j++) {
-
-    field.append('<div class="result question">'+base.bullet[j].question+'</div>');
+    field.append('<div class="result question">'+base.bullet[j].question+'</div>');//print question
     
     if (base.bullet[j].picture != '') {
-    field.append('<img src="'+base.bullet[j].picture+'" class="result picture">');
+    field.append('<img src="'+base.bullet[j].picture+'" class="result picture">');//print img
     }
+    let your;
 
-    for (let n =0; n < 4; n++ ) {
-      field.append('<div class="result answers">'+base.bullet[j].answers[n]+'</div>');
+    if ((base.bullet[j].typeOFanswers == "button")) {
+
+     for (let n =0; n < 4; n++ ) {
+      
+           
+        if (base.bullet[j].answers[n] == baseResult[j+1]) {
+          your = " (Ваш ответ)";
+        } else {
+          your = "";
+        }
+              //mark right
+        if (base.bullet[j].answers[n] == base.bullet[j].right_answer) {
+          field.append('<div class="result answers green">'+base.bullet[j].answers[n]+'<span class="blue">'+your+'</span>'+'</div>');
+
+          //  Check right answer
+          if (base.bullet[j].right_answer == baseResult[j+1]) { total_right++ ; }
+          
+        } else {
+          field.append('<div class="result answers">'+base.bullet[j].answers[n]+'<span class="blue">'+your+'</span>'+'</div>');
+        }
+      }
     }
-
     field.append('<div class="line"></div>');
-
-    
   }
 
+  // *** Print - input *********************
+
+
+
+
+
+  //****************** TOTAL ********************** */
+
+  field.append('<div class="total_right"> ПРАВИЛЬНЫХ ОТВЕТОВ :  '+total_right+'</div>');
 }
